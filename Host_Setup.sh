a5858d67073614d5926e290dc8f3145a0bd534ad
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-05-04 22:42:53 +0200

    Update Host_Setup.sh

--- Host_Setup.sh
+++ Host_Setup.sh
@@ -1,31 +1,29 @@
-#Basic Setup
+# 1. Mettre à jour la machine hôte (Debian 10)
 sudo apt -y update
 sudo apt -y upgrade
 
-#Tools for Host (Host is a Debian 10 VMware VM)
+# 2. Installer les outils d'intégration VMware (si la machine hôte est une VM VMware neuve)
 sudo apt install -y open-vm-tools-desktop
 
-#Curl
+# 3. Un outil de téléchargement en ligne de commande : cURL
 sudo apt install -y curl
 
-#Visual Studio Code
+# 4. Un éditeur de texte : VSCode
 curl https://az764295.vo.msecnd.net/stable/ff915844119ce9485abfe8aa9076ec76b5300ddd/code_1.44.2-1587059832_amd64.deb --output /home/$USER/Downloads/code.deb
 sudo dpkg -i /home/$USER/Downloads/code.deb
 
-#Vagrant Virtualbox
+# 5. Un hyperviseur : Virtualbox
 echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian bionic contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 sudo apt update
 sudo apt install -y virtualbox-6.0
 
-#Vagrant 2.2.7
+# 6. Un outil de création d'environnement pour machines virtuelles : Vagrant
 curl https://releases.hashicorp.com/vagrant/2.2.7/vagrant_2.2.7_x86_64.deb --output /home/$USER/Downloads/vagrant-2.2.7.deb
 sudo dpkg -i /home/$USER/Downloads/vagrant-2.2.7.deb
 vagrant plugin install vagrant-vbguest
 mkdir vagrant_projet_3
 cd vagrant_projet_3
 
-
-#Vagrant Bootstrap Setup : Visual Studio Code : See bootstrap.sh
-#Vagrant Bootstrap Setup : Ansible - Latest : See bootstrap.sh
-#Vagrant Bootstrap Setup : Docker CE - Latest : See bootstrap.sh
+# 7. Les outils Virtualbox permettant le partage de fichiers entre la machine hôte et la machine virtuelle Vagrant
+vagrant init generic/debian10
