8cd95ca87c35459dfba5e95e22ca68cfd03b596b
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-05-04 20:45:26 +0200

    Update Vagrant_Setup.sh

--- Vagrant_Setup.sh
+++ Vagrant_Setup.sh
@@ -1,3 +1,6 @@
+#Vagrant Plugin for box generic/debian10
+vagrant plugin install vagrant-vbguest
+
 #Vagrant Config Setup
 vagrant init generic/debian10
 vagrant box add generic/debian10 --provider=virtualbox
