77db807e3fcccf7b539edaebbe8ae3ce344ff4ed
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-05-14 22:28:53 +0200

    Update Projet_3.sh

--- Projet_3.sh
+++ Projet_3.sh
@@ -40,13 +40,13 @@ echo 'sudo apt install -y apt-transport-https ca-certificates curl gnupg2 softwa
 echo 'curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -' >> bootstrap.sh
 echo 'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"' >> bootstrap.sh
 
-# 4.4 Vagrant Bootstrap Setup
-echo 'sudo apt update -y
-echo 'sudo apt install -y apt-transport-https gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
-echo 'sudo apt install -y code
-echo 'sudo apt install -y ansible
-echo 'sudo apt install -y docker docker-ce docker-ce-cli containerd.io
-echo 'sudo usermod -aG docker vagrant
+# 4.4 Vagrant Bootstrap Setup' >> bootstrap.sh
+echo 'sudo apt update -y' >> bootstrap.sh
+echo 'sudo apt install -y apt-transport-https gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget' >> bootstrap.sh
+echo 'sudo apt install -y code' >> bootstrap.sh
+echo 'sudo apt install -y ansible' >> bootstrap.sh
+echo 'sudo apt install -y docker docker-ce docker-ce-cli containerd.io' >> bootstrap.sh
+echo 'sudo usermod -aG docker vagrant' >> bootstrap.sh
 
 # 4.5 Vagrant Bootstrap Docker startup
 
