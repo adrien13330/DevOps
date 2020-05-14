50c56e7e6b2cf6b6fc44004e82f58422914262b2
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-05-14 22:01:25 +0200

    Update Projet_3_Adrien.sh

--- Projet_3_Adrien.sh
+++ Projet_3_Adrien.sh
@@ -26,37 +26,31 @@ echo 'CMD ["nginx", "-g", "daemon off;"]' >> Dockerfile
 
 # 4. bootstrap.sh
 
-# 4.1 VSCode
+# 4.1 Vagrant Bootstrap Setup: VSCode
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-echo 'sudo apt -y update' >> bootstrap.sh
-echo 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg' >> bootstrap.sh
-echo 'sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/' >> bootstrap.sh
-echo $'sudo sh -c \'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list\'' >> bootstrap.sh
-echo 'sudo apt-get install -y apt-transport-https' >> bootstrap.sh
-echo 'sudo apt-get update -y' >> bootstrap.sh
-echo 'sudo apt-get install -y code' >> bootstrap.sh
-echo 'sudo apt install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget' >> bootstrap.sh
+# 4.2 Vagrant Bootstrap Setup: Ansible - Latest
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 
-# 4.2 Ansible - Latest
+# 4.3 Vagrant Bootstrap Setup: Docker - Latest
+sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
-echo 'echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list' >> bootstrap.sh
-echo 'sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367' >> bootstrap.sh
-echo 'sudo apt update -y' >> bootstrap.sh
-echo 'sudo apt install ansible -y' >> bootstrap.sh
+# 4.4 Vagrant Bootstrap Setup
+sudo apt update -y
+sudo apt install -y apt-transport-https gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+sudo apt install -y code
+sudo apt install -y ansible
+sudo apt install -y docker docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker vagrant
 
-# 4.3 Docker CE - Latest
+# 4.5 Vagrant Bootstrap Docker startup
 
-echo 'sudo apt update -y' >> bootstrap.sh
-echo 'sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common' >> bootstrap.sh
-echo 'curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -' >> bootstrap.sh
-echo 'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"' >> bootstrap.sh
-echo 'sudo apt update -y' >> bootstrap.sh
-echo 'sudo apt install -y docker docker-ce docker-ce-cli containerd.io' >> bootstrap.sh
-echo 'sudo usermod -aG docker vagrant' >> bootstrap.sh
-
-# 4.4 Docker startup from bootstrap
-
-#echo 'cp /vagrant/Dockerfile $HOME/.' >> bootstrap.sh
+echo 'cp /vagrant/Dockerfile $HOME/Dockerfile' >> bootstrap.sh
 echo 'docker pull debian' >> bootstrap.sh
 echo 'docker build -t test .' >> bootstrap.sh
 echo 'docker run -dit -p 80:80 test' >> bootstrap.sh
@@ -66,3 +60,6 @@ echo 'docker run -dit -p 80:80 test' >> bootstrap.sh
 #vagrant init generic/debian10
 vagrant box add generic/debian10 --provider=virtualbox
 vagrant up
+
+# Test
+curl 172.17.0.2
