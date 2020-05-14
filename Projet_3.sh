8dd5fe37298a1a5bc31aedecee7931e5e19bbfb9
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-05-14 22:49:02 +0200

    Update Projet_3.sh

--- Projet_3.sh
+++ Projet_3.sh
@@ -11,7 +11,7 @@ echo '  config.vm.synced_folder "", "/vagrant"' >> Vagrantfile
 echo '  config.vm.provision :shell, path: "bootstrap.sh"' >> Vagrantfile
 echo '  config.vm.network "forwarded_port", guest: 80, host: 8080' >> Vagrantfile
 echo '  config.vm.provider "virtualbox" do |v|' >> Vagrantfile
-echo '  config.vm.provision "file", source: "Dockerfile", destination: "$HOME/*"' >> Vagrantfile
+echo '  config.vm.provision "file", source: "Dockerfile", destination: "$HOME/Dockerfile"' >> Vagrantfile
 echo '    v.memory = 4096' >> Vagrantfile
 echo '  end' >> Vagrantfile
 echo 'end' >> Vagrantfile
@@ -50,7 +50,7 @@ echo 'sudo usermod -aG docker vagrant' >> bootstrap.sh
 
 # 4.5 Vagrant Bootstrap Docker startup
 
-echo 'cp /vagrant/Dockerfile /home/vagrant/Dockerfile' >> bootstrap.sh
+#echo 'cp /vagrant/Dockerfile /home/vagrant/Dockerfile' >> bootstrap.sh
 echo 'docker pull debian' >> bootstrap.sh
 echo 'docker build -t test .' >> bootstrap.sh
 echo 'docker run -dit -p 80:80 test' >> bootstrap.sh
