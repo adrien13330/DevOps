d8a7bbef384c3e3fee2dabd588a41435cdc7bd02
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-05-15 11:29:26 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,7 +27,7 @@ sudo apt install -y docker docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker vagrant
 docker pull debian
-docker build -t prod /vagrant/.
-docker run -dit -p 2222:22 -p 80:80 prod
+docker build -t Projet_03 /vagrant/.
+docker run -dit -p 2222:22 -p 80:80 Projet_03
 
 
