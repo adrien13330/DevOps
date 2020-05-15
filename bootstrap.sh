bd1a5a15589e8f9286524a2d000c0dbe7611a75c
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-05-15 11:34:28 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-pro
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
-#Vagrant Bootstrap Setup : VSCode + Ansible + Docker CE
+#Vagrant Bootstrap Setup: VSCode + Ansible + Docker CE
 
 sudo apt update -y
 sudo apt install -y apt-transport-https gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
@@ -23,7 +23,7 @@ sudo apt install -y code
 sudo apt install -y ansible
 sudo apt install -y docker docker-ce docker-ce-cli containerd.io
 
-#Vagrant Docker Setup
+#Vagrant Bootstrap Docker: Startup
 
 sudo usermod -aG docker vagrant
 docker pull debian
