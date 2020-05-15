df41d2bc07138b19f11c8af31f425bd6b787d470
adrien13330 <64000207+adrien13330@users.noreply.github.com>
2020-05-15 11:11:41 +0200

    Update Projet_3.sh

--- Projet_3.sh
+++ Projet_3.sh
@@ -26,20 +26,24 @@ echo 'CMD ["nginx", "-g", "daemon off;"]' >> Dockerfile
 # 4. bootstrap.sh
 
 # 4.1 Vagrant Bootstrap Setup: VSCode
+
 echo 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg' >> bootstrap.sh
 echo 'sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/' >> bootstrap.sh
 echo $'sudo sh -c \'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list\'' >> bootstrap.sh
 
 # 4.2 Vagrant Bootstrap Setup: Ansible - Latest
+
 echo 'echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list' >> bootstrap.sh
 echo 'sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367' >> bootstrap.sh
 
 # 4.3 Vagrant Bootstrap Setup: Docker - Latest
+
 echo 'sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common' >> bootstrap.sh
 echo 'curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -' >> bootstrap.sh
 echo 'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"' >> bootstrap.sh
 
 # 4.4 Vagrant Bootstrap Setup: Install
+
 echo 'sudo apt update -y' >> bootstrap.sh
 echo 'sudo apt install -y apt-transport-https gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget' >> bootstrap.sh
 echo 'sudo apt install -y code' >> bootstrap.sh
