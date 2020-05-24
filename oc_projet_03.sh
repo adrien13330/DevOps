1092dc10d8c5cd75431414eace6bd7062c801c4c
adrien13330 <adrien13330@orange.fr>
2020-05-24 22:17:36 +0200

    curl from git

--- oc_projet_03.sh
+++ oc_projet_03.sh
@@ -1,8 +1,6 @@
 # 1. Host machine environment setup
 
 timer=`date +%Y-%m-%d_%H:%M:%S`
-docker_env=$HOME/GitHub/Projet_03_Adrien_Docker/
-vagrant_env=$HOME/GitHub/Projet_03_Adrien_Vagrant/
 prod_env=$HOME/oc_projet_03_$timer/
 
 mkdir $prod_env
@@ -10,12 +8,12 @@ cd $prod_env
 
 # 2. Vagrant
 
-cp $vagrant_env/Vagrantfile $prod_env/
-cp $vagrant_env/bootstrap.sh $prod_env/
+curl https://raw.githubusercontent.com/adrien13330/Projet_03_Adrien_Vagrant/master/Vagrantfile --output Vagrantfile
+curl https://raw.githubusercontent.com/adrien13330/Projet_03_Adrien_Vagrant/master/bootstrap.sh --output bootstrap.sh
 
 # 3. Docker
 
-cp $docker_env/Dockerfile  $prod_env/
+curl https://raw.githubusercontent.com/adrien13330/Projet_03_Adrien_Docker/master/Dockerfile --output Dockerfile
 
 # 4. Vagrant up
 
