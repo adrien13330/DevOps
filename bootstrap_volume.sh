815bcf0cdfc22c182fbf19a2dcbee089ab12cac3
adrien13330 <adrien13330@orange.fr>
2020-05-28 00:59:50 +0200

    Volume

---
+++ bootstrap_volume.sh
@@ -0,0 +1,34 @@
+#Vagrant Bootstrap Repository Setup: VSCode
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+#Vagrant Bootstrap Repository Setup: Ansible - Latest
+
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+
+#Vagrant Bootstrap Repository Setup: Docker CE - Latest
+
+sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
+#Vagrant Bootstrap Setup: VSCode + Ansible + Docker CE
+
+sudo apt update -y
+sudo apt install -y apt-transport-https gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+sudo apt install -y code
+sudo apt install -y ansible
+sudo apt install -y docker docker-ce docker-ce-cli containerd.io
+
+#Vagrant Bootstrap Docker CE: Run
+
+sudo usermod -aG docker vagrant
+docker pull debian
+curl https://raw.githubusercontent.com/adrien13330/Projet_03_Adrien_Docker/master/Dockerfile -so Dockerfile
+docker build -t oc_projet_03 .
+docker run -dit -p 2222:22 -p 80:80 oc_projet_03
+
+
