40c43c23516bbeda30860d77974bea15ee6d2aa0
Adrien <adrien13330@hotmail.com>
2020-06-12 17:02:54 +0200

    Projet 04

--- Host_Setup_Ubuntu.sh
+++ Host_Setup_Ubuntu.sh
@@ -34,7 +34,7 @@ sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent softwa
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-# 5.3 Facultatif : Docker Compose
+# 6. Projet 04 : Docker Compose
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
